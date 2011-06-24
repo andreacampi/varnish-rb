d557d4f8d41f616774464229420767b4ffa335fc
Andrea Campi <andrea.campi@zephirworks.com>
2011-06-24 13:04:25 +0200

    Cleanup: split the VSL enum out to its own module.

--- lib/varnish/vsl.rb
+++ lib/varnish/vsl.rb
@@ -1,78 +1,14 @@
 require 'varnish'
 require 'ffi'
+require 'varnish/vsl/enum'
 
 module Varnish
   module VSL
     extend FFI::Library
     ffi_lib Varnish::LIBVARNISHAPI
+    include Enum
 
-    enum :vsl_tag, [
-      :debug,
-      :error,
-      :cli,
-      :statsess,
-      :reqend,
-      :sessionopen,
-      :sessionclose,
-      :backendopen,
-      :backendxid,
-      :backendreuse,
-      :backendclose,
-      :httpgarbage,
-      :backend,
-      :length,
-
-      :fetcherror,
-
-      :rxrequest,
-      :rxresponse,
-      :rxstatus,
-      :rxurl,
-      :rxprotocol,
-      :rxheader,
-
-      :txrequest,
-      :txresponse,
-      :txstatus,
-      :txurl,
-      :txprotocol,
-      :txheader,
-
-      :objrequest,
-      :objresponse,
-      :objstatus,
-      :objurl,
-      :objprotocol,
-      :objheader,
-
-      :lostheader,
-
-      :ttl,
-      :fetch_body,
-      :vcl_acl,
-      :vcl_call,
-      :vcl_trace,
-      :vcl_return,
-      :vcl_error,
-      :reqstart,
-      :hit,
-      :hitpass,
-      :expban,
-      :expkill,
-      :workthread,
-
-      :esi_xmlerror,
-
-      :hash,
-
-      :backend_health,
-      :vcl_log,
-
-      :gzip
-
-      ]
-
-    callback :VSL_handler_f, [:pointer, :vsl_tag, :int, :int, :int, :pointer, :int64], :int
+    callback :VSL_handler_f, [:pointer, VslTag, :int, :int, :int, :pointer, :int64], :int
 
     attach_function 'VSL_Setup',    [ :pointer ], :void
     attach_function 'VSL_Open',     [ :pointer, :int ], :int
