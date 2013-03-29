93bd49f095d9ce4958f40adec52fc0dc23839921
Andrea Campi <andrea.campi@zephirworks.com>
2013-03-29 22:33:28 +0100

    [#2] Provide alternative names for the varnishapi shared library to support more platforms.

--- lib/varnish.rb
+++ lib/varnish.rb
@@ -1,5 +1,8 @@
 module Varnish
-  LIBVARNISHAPI = 'libvarnishapi.1'
+  LIBVARNISHAPI = [
+    'libvarnishapi.1',    # Mac OS X
+    'libvarnishapi.so.1'  # Debian / Ubuntu
+  ]
 end
 
 require 'varnish/vsm'
